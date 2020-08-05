a56f776ea88d3feef4c98673a6d32247da00733c
Gabriel Rezende <gmrezende91@gmail.com>
2020-08-05 14:17:37 -0300

    configurando jenkins

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 const routes = require("express").Router();
 
 routes.get("/", (req, res) => {
-  return res.status(200).send({'message':'API Ok'});
+  return res.status(200).send({ message: "API Ok" });
+});
+
+routes.get("/count", (req, res) => {
+  let a = 0;
+  let b = 0;
+  let c = a + b;
+  c = a + b;
+  return res.status(200).send({ message: "API Ok", count: c });
 });
 
 module.exports = routes;
